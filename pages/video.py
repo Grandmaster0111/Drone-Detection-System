7c02dc75816908f252a6385c4d0d459be5371e3d
Grandmaster0111 <shubhamssg77@gmail.com>
2024-05-15 07:15:08 +0530

    add venv and video file

---
+++ pages/video.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import cv2
+import tempfile
+
+st.title('Drone Detection System')
+
+vf = st.file_uploader('Upload your desire Video',type=['mp4'])
+if vf is None:
+    st.write('File is missing')
+else:
+    tfile = tempfile.NamedTemporaryFile(delete=False)
+    tfile.write(vf.read())
+    cap = cv2.VideoCapture(tfile.name)
+
+    stframe = st.empty()
+
+    while cap.isOpened():
+        ret,frame = cap.read()
+        if not ret:
+            print('cant open')
+            break
+        gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        stframe.image(gray)
